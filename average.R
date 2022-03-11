fc41ad565fc8a4b58f315497d34e72607b94989d
Hacker-is-back <nelash73@gmail.com>
2022-03-11 22:00:51 +0530

    edit

--- average.R
+++ average.R
@@ -1,6 +1,5 @@
 year<- c(1986,1987,1988,1989,1990,1991)
 production <- c(1542,1442,1552,2102,2612,3195)
-df <- data.frame(Year=year,Production=production,
-stringsAsFactors = FALSE)
+df <- data.frame(Year=year,Production=production)
 print(df)
 print(mean(df$Production))
